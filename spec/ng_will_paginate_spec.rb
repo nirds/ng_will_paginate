5129489df7de11503608808bd829cdd7c5760940
Cheri Allen <cherimarie@gmail.com>
2016-01-08 11:54:52 -0800

    First test

--- spec/ng_will_paginate_spec.rb
+++ spec/ng_will_paginate_spec.rb
@@ -5,7 +5,17 @@ describe NgWillPaginate do
     expect(NgWillPaginate::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'objectifies and returns reasonable content when there are no records' do
+    per_page = 10
+    page = 1
+    records = []
+    expect(NgWillPaginate::Objectifier.JSONify(per_page, page, records)).to eq({:max=>0, :records=>[]})
+  end
+
+  it 'objectifies and returns reasonable content when there are multiple pages of records' do
+    per_page = 3
+    page = 2
+    records = []
+    expect(NgWillPaginate::Objectifier.JSONify(per_page, page, records)).to eq({:max=>0, :records=>[4, 5, 6]})
   end
 end
